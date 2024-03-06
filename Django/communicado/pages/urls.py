0d96eb2f2ed9ec5f428c9a14968b876595cf661b
Sparsh khanna <97553311+Sparshkhannaa@users.noreply.github.com>
2024-03-06 01:57:23 -0800

    Signup Page
    Have Implemented and coded a basic signup page

--- Django/communicado/pages/urls.py
+++ Django/communicado/pages/urls.py
@@ -3,5 +3,6 @@ from pages import views
 
 urlpatterns = [
     path("", views.home, name='home'),
-    path("login/",views.login , name = 'login')
+    path("login/",views.login , name = 'login'),
+    path("signup/", views.signup , name ='signup')
 ]
