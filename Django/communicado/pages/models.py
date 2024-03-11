95d015d2d42b9a6b1b158ab338c2bb3c8a69d922
Sparsh khanna <97553311+Sparshkhannaa@users.noreply.github.com>
2024-03-11 16:35:27 -0700

    Events /Customer List
    Created models /views/pages in order to query from our database to display a simple list of data from our database

--- Django/communicado/pages/models.py
+++ Django/communicado/pages/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
 # Create your models here.
+class users(models.Model):
+    userID = models.AutoField(primary_key=True)
+    role = models.CharField(max_length=50)
+    username = models.CharField(max_length=100, unique=True)
+    email = models.EmailField(unique=True)
+    address = models.CharField(max_length=200)
+    password = models.CharField(max_length=100)
+
+    class Meta:
+        db_table = 'users'
+class events(models.Model):
+    eventID = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    eventDateTime = models.DateTimeField()
+    location = models.CharField(max_length=100, blank=True, null=True)
+    capacity = models.IntegerField(blank=True, null=True)
+    category = models.CharField(max_length=50, blank=True, null=True)
+    artist = models.CharField(max_length=100, blank=True, null=True)
+    isVerified = models.BooleanField(default=False)
+    #admin = models.ForeignKey(users, on_delete=models.CASCADE, related_name='admin_events', blank=True, null=True)
+    #eventOrganizer = models.ForeignKey(users, on_delete=models.CASCADE, related_name='organizer_events', blank=True, null=True)
+    class Meta:
+        db_table = 'events'
