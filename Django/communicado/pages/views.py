07ddedc693edca34813c6b52088fceded418a5a3
MithishR <mithishrg@gmail.com>
2024-03-12 16:14:40 -0700

    Added the GET/POST functionality for signup page, TBD for login page

--- Django/communicado/pages/views.py
+++ Django/communicado/pages/views.py
@@ -1,13 +1,31 @@
 from django.shortcuts import render, redirect
 from .models import users
 from .models import events
+from django.views.decorators.csrf import csrf_exempt
 
 def home(request):
     return render(request, "pages/home.html", {})
 def login(request):
-    return render(request, "pages/login.html", {})
+    
+    # if request.method == 'POST':
+    #     username = request.POST.get('username')
+    #     password = request.POST.get('password')
+    #     return redirect('login')
+    # else:
+        
+        return render(request, "pages/login.html", {})
+    
+@csrf_exempt
 def signup(request):
-    return render(request, "pages/signup.html", {})
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+        return redirect('login')
+    else:
+        return render(request, "pages/signup.html", {})
+
+    # return render(request, "pages/signup.html", {})
 def useracc(request):
     data = users.objects.all()
     context = {"users": data}
