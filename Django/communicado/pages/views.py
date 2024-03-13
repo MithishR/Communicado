f9d3f56a6e211e77d612d3138a65b501c614ca14
mahigangal <gangalmahi@gmail.com>
2024-03-12 18:22:48 -0700

    temp changes

--- Django/communicado/pages/views.py
+++ Django/communicado/pages/views.py
@@ -1,11 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from .models import users
 from .models import events
+from django.contrib.auth import authenticate, login as auth_login
+
 
 def home(request):
     return render(request, "pages/home.html", {})
 def login(request):
-    return render(request, "pages/login.html", {})
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            auth_login(request, user)
+            return redirect('home')  # Redirect to home page after successful login
+        else:
+            error_message = "Invalid username or password."
+            return render(request, 'login.html', {'error_message': error_message})
+    else:
+        return render(request, 'login.html')
 def signup(request):
     return render(request, "pages/signup.html", {})
 def useracc(request):
