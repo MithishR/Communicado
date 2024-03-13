f4ce840287196f2eb43f18d9a57aa9e5ef97ab55
prathamkshah13 <prathamkshah130503@gmail.com>
2024-03-12 21:58:43 -0700

    Modules and admin updated

--- Django/communicado/pages/admin.py
+++ Django/communicado/pages/admin.py
@@ -1,5 +1,32 @@
+# admin.py
+
 from django.contrib import admin
-from .models import *
-# Register your models here.
-admin.site.register(users)
-admin.site.register(events)
+from .models import users, events, EventOrganizer, Customer, BookedEvent
+
+class UsersAdmin(admin.ModelAdmin):
+    list_display = ('userID', 'role', 'username', 'email', 'address')
+    search_fields = ('username', 'email')
+
+class EventsAdmin(admin.ModelAdmin):
+    list_display = ('eventID', 'name', 'eventDateTime', 'location', 'capacity', 'category', 'artist', 'isVerified', 'adminID', 'eventOrganizerID')
+    search_fields = ('name', 'category', 'artist')
+    list_filter = ('isVerified',)
+
+class EventOrganizerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phoneNumber')
+    search_fields = ('user__username', 'phoneNumber')
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'phoneNumber')
+    search_fields = ('user__username', 'phoneNumber')
+
+class BookedEventAdmin(admin.ModelAdmin):
+    list_display = ('eventID', 'quantity', 'isPaid', 'user', 'referenceNumber')
+    search_fields = ('eventID__name', 'referenceNumber')
+    list_filter = ('isPaid',)
+
+admin.site.register(users, UsersAdmin)
+admin.site.register(events, EventsAdmin)
+admin.site.register(EventOrganizer, EventOrganizerAdmin)
+admin.site.register(Customer, CustomerAdmin)
+admin.site.register(BookedEvent, BookedEventAdmin)
