d1145798363caf59625b1f9e61dab9d9058a4faa
mahigangal <gangalmahi@gmail.com>
2024-03-20 15:10:27 -0700

    passwords encryption and decryption added

--- Django/communicado/pages/views.py
+++ Django/communicado/pages/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,redirect,get_object_or_404
+from django.contrib.auth.hashers import make_password,check_password
 from .models import *
 
 
@@ -26,14 +27,15 @@ def login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
         password = request.POST.get('password')
+        
         try:
-            user = users.objects.get(username=username, password=password)
-            # Authentication successful
-            if user:
-                # Redirect to home page or any other page you want
+            user = users.objects.get(username=username)
+            
+            # Check if the entered password matches the hashed password in the database
+            if check_password(password, user.password):
+                # Authentication successful
                 success_message = "Welcome " + user.name  # Accessing name from the user object
                 return render(request, 'pages/home.html', {'success_message': success_message})
-                
             else:
                 error_message = "Invalid username or password."
                 return render(request, 'pages/login.html', {'error_message': error_message})
@@ -44,6 +46,7 @@ def login(request):
         return render(request, 'pages/login.html')
 
 
+
 def signup(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -52,7 +55,11 @@ def signup(request):
         email = request.POST.get('email')
         role=request.POST.get('role')
         address=request.POST.get('address')
-        user = users(role=role, name = name, username=username, email=email, address=address, password=password)
+        
+        # Encrypt the password
+        encrypted_password = make_password(password)
+        
+        user = users(role=role, name = name, username=username, email=email, address=address, password=encrypted_password)
         user.save()
         success_message = "User Account Created for: " + user.name  # Accessing name from the user object
         return render(request, 'pages/login.html', {'success_message': success_message})
