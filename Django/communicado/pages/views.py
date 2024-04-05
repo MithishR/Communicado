88edbc7c5b39a1c20c61d0d4f7db1e5878559968
Sparsh khanna <97553311+Sparshkhannaa@users.noreply.github.com>
2024-04-05 02:43:03 -0700

    Fixed Bugs and added functionality
    The user can now view their booking history !
    Fixed Bugs that were preventing this page from rendering .
    Added connectivity with user details page so a user cna see the page there

--- Django/communicado/pages/views.py
+++ Django/communicado/pages/views.py
@@ -218,11 +218,13 @@ def userbookeventinfo(request):
         return render(request, 'pages/userbookinghistory.html', {'error_message': error_message, 'show_login_button': True})
     else:
         booked_events = BookedEvent.objects.filter(user=userId)
-        if not booked_events:
+        if booked_events.count() == 0 :
             no_events_message = "You currently have no booked events. Book now!"
             return render(request, 'pages/userbookinghistory.html', {'no_events_message': no_events_message, 'show_events_button': True})
         else:
-            return render(request, 'pages/userbookinghistory.html', {'booked_events': booked_events})
+            eventslist =   booked_events.values_list('eventID',flat = True)
+            events = Events.objects.filter(eventID__in=eventslist)
+            return render(request, 'pages/userbookinghistory.html', {'events': events})
 
         
        
