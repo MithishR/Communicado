0397bb96eb7f1dfb47c63d538d62750814e734ee
Ojus Sharma <ojus@student.ubc.ca>
2024-04-07 02:44:45 -0700

    Created universal header file
    - Created header.html that is sourced by all other pages to ensure a smooth and consistent ui experience

--- Django/communicado/pages/views.py
+++ Django/communicado/pages/views.py
@@ -25,6 +25,7 @@ def login(request):
             user = users.objects.get(username=username)
             if check_password(password, user.password):
                 request.session['userID'] = user.userID
+                request.session['userRole'] = user.role
                 #request.session.set_test_cookie() 
                 if user.role == 'EventOrganizer':
                     success_message = "Welcome " + user.name + ", userid:"+ str(user.userID) # Accessing name from the user object
